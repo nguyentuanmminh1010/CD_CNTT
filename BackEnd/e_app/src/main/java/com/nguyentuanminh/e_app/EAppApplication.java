444ad018dadd0ba41d46334c65a3e5129e99cd31
nguyentuanminh1010 <t.minh10102005@gmail.com>
2026-09-16 12:41:09 +0700

    Initial commit

---
+++ BackEnd/e_app/src/main/java/com/nguyentuanminh/e_app/EAppApplication.java
@@ -0,0 +1,13 @@
+package com.nguyentuanminh.e_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAppApplication.class, args);
+	}
+
+}
